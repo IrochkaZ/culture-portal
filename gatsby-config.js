11125f8e4121f3e92111dc19b7e594caef87cadd
andreikrivets <36642792+andreikrivets@users.noreply.github.com>
2020-02-17 21:36:19 +0300

    init: basic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,20 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sharp`
+    //   `gatsby-plugin-react-helmet`,
+    //   {
+    //     resolve: `gatsby-source-filesystem`,
+    //     options: {
+    //       name: `images`,
+    //       path: `${__dirname}/src/images`,
+    //     },
+    //   },
+
     // {
     //   resolve: `gatsby-plugin-manifest`,
     //   options: {
@@ -30,5 +31,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
