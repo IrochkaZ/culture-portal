83f9d18a05972340c9c665f1b9250fdf3fae3969
Andrei Krivets <etopushka@gmail.com>
2020-02-19 10:43:35 +0300

    fix: add second parameter into find-entries block

--- src/components/index/getMainPageData.jsx
+++ src/components/index/getMainPageData.jsx
@@ -1,12 +1,11 @@
 const contentful = require("contentful");
 
-const getMainPageData = async language => {
+const getMainPageData = async (language, name) => {
   const client = contentful.createClient({
     space: "fk0dvhht7mqc",
     environment: "raprap",
     accessToken: "F0B2TsvVUAi122zbpjBc8zMc-dTVM1BV6ht3DzuuyHo"
   });
-  const name = "verba";
   const data = await client.getEntries({
     content_type: "verba",
     "fields.lang[match]": `${language}`,
