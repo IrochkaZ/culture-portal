84958d493c6119f63b4e58639e2745c1f24b660a
Irina <zaletk.irina@gmail.com>
2020-02-27 16:49:05 +0300

    refactor: add unique key gallery

--- src/components/pageAuthor/gallery.jsx
+++ src/components/pageAuthor/gallery.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import uniquid from "uniquid";
 import "./gallery.css";
 
 export default function Gallery(data) {
   const { pageinfo } = data;
   const imagesArray = data.data;
-  global.console.log(imagesArray);
   const img = imagesArray.map(item => (
-    <div className="gallery-wrapper">
+    <div className="gallery-wrapper" key={uniquid()}>
       <img className="gallery-img" src={item} />
     </div>
   ));
