927e68ce01e9f8c08d445456046bdd94b4e27019
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2020-02-27 17:42:14 +0300

    fix: use eslint & correct lang button bug

--- src/pages/poets.jsx
+++ src/pages/poets.jsx
@@ -1,54 +1,44 @@
-import React, { Component, Fragment } from "react";
+import React, { useState } from "react";
 import SearchInput, { createFilter } from "react-search-input";
-import Header from "../components/common/header";
 import { Link } from "gatsby";
+import Header from "../components/common/header";
 
 const poetsData = JSON.parse(localStorage.poetsData).slice(1); // exclude project members [0] from poets array
 
-class Poets extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: ""
-    };
-    this.searchUpdated = this.searchUpdated.bind(this);
-  }
-
-  render() {
-    const lang = localStorage.lang;
-    const KEYS_TO_FILTERS = [
-      `fields.data.${lang}.city`,
-      `fields.data.${lang}.name`
-    ];
-    const filteredPoets = poetsData.filter(
-      createFilter(this.state.searchTerm, KEYS_TO_FILTERS)
-    );
-    return (
-      <Fragment>
-        <Header setLanguage={e => setLanguage(e.target.value)} />
-        <div>
-          <SearchInput className="search-input" onChange={this.searchUpdated} />
-          {filteredPoets.map(poet => {
-            const { data } = poet.fields;
-            return (
-              <div className="result" key={poet.fields.id}>
-                <Link className="summary" to={`poet/${poet.fields.id}`}>
-                  <p className="poet-name">
-                    {data[lang].name}{" "}
-                    <span className="poet-summary">{data[lang].summary}</span>
-                  </p>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      </Fragment>
-    );
-  }
-
-  searchUpdated(term) {
-    this.setState({ searchTerm: term });
-  }
-}
+const Poets = () => {
+  const [lang, setLang] = useState(localStorage.getItem("lang"));
+  const [searchTerm, setSearchTerm] = useState("");
+  const KEYS_TO_FILTERS = [
+    `fields.data.${lang}.city`,
+    `fields.data.${lang}.name`
+  ];
+  const filteredPoets = poetsData.filter(
+    createFilter(searchTerm, KEYS_TO_FILTERS)
+  );
+  return (
+    <>
+      <Header setLanguage={e => setLang(e.target.value)} />
+      <div>
+        <SearchInput
+          className="search-input"
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        {filteredPoets.map(poet => {
+          const { data } = poet.fields;
+          return (
+            <div className="result" key={poet.fields.id}>
+              <Link className="summary" to={`poet/${poet.fields.id}`}>
+                <p className="poet-name">
+                  {data[lang].name}
+                  <span className="poet-summary">{data[lang].summary}</span>
+                </p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Poets;
