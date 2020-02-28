93ef13c4b4dbebdf3f9e6ddda40bceb1bd12e724
Andrei Krivets <etopushka@gmail.com>
2020-02-28 12:15:28 +0300

    fix: author page

--- src/pages/poet.jsx
+++ src/pages/poet.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
 import WriterCard from "../components/pageAuthor/writercard";
 import ListAutors from "../components/pageAuthor/listAutors";
 import Timelines from "../components/pageAuthor/timelines";
@@ -7,66 +7,25 @@ import Gallery from "../components/pageAuthor/gallery";
 import Video from "../components/pageAuthor/video";
 import Maps from "../components/pageAuthor/maps";
 import Header from "../components/common/header";
-import getMainPageData from "../components/index/getMainPageData";
 
-export default function Poet({ poetData }) {
-  const [datas, setData] = useState({});
-  const [language, setLang] = useState(localStorage.getItem("lang"));
-  useEffect(() => {
-    async function fetchData() {
-      const response = await getMainPageData();
-      setData(response);
-    }
-    fetchData();
-  }, []);
+export default function Poet() {
+  const [language, setLanguage] = useState(localStorage.getItem("lang"));
 
-  if (!datas.length) return null;
-  const pageInfos = datas.filter(el => el.fields.lang === "mainInfo");
-  localStorage.setItem("pageInfo", JSON.stringify(pageInfos[0].fields.data));
-  const pageMainInfo = pageInfos[0].fields.data[language];
-  localStorage.setItem("lang", language);
-  localStorage.setItem("buttons", pageMainInfo.buttons);
+  const dataString = localStorage.getItem("poetOfTheDayData");
+  if (!dataString) return <h2>Error</h2>;
+  const poetData = JSON.parse(dataString.split(",")).data;
+  const pageInfo = JSON.parse(localStorage.getItem("pageInfo"));
 
-  localStorage.setItem(
-    "poetOfTheDayData",
-    JSON.stringify(JSON.parse(localStorage.getItem("poetOfTheDayData")))
-  );
-  localStorage.setItem("poetsData", JSON.stringify(pageMainInfo));
-
+  if (!Object.keys(poetData).length) return null;
   return (
     <>
-      <Header setLanguage={e => setLang(e.target.value)} />
+      <Header setLanguage={e => setLanguage(e.target.value)} />
       <WriterCard data={poetData[language]} />
-      <ListAutors
-        data={poetData[language]}
-        pageinfo={pageInfos[0].fields.data[language]}
-      />
-      <Timelines
-        data={poetData[language]}
-        pageinfo={pageInfos[0].fields.data[language]}
-      />
-      <Gallery
-        data={poetData.images}
-        pageinfo={pageInfos[0].fields.data[language]}
-      />
-      <Video
-        data={poetData.video}
-        pageinfo={pageInfos[0].fields.data[language]}
-      />
-      <Maps
-        data={poetData.coords}
-        pageinfo={pageInfos[0].fields.data[language]}
-      />
+      <ListAutors data={poetData[language]} pageinfo={pageInfo[language]} />
+      <Timelines data={poetData[language]} pageinfo={pageInfo[language]} />
+      <Gallery data={poetData.images} pageinfo={pageInfo[language]} />
+      <Video data={poetData.video} pageinfo={pageInfo[language]} />
+      <Maps data={poetData.coords} pageinfo={pageInfo[language]} />
     </>
   );
 }
-
-const lang = localStorage.getItem("lang");
-
-const poetData = JSON.parse(localStorage.getItem("poetOfTheDayData").split(","))
-  .data;
-
-Poet.defaultProps = {
-  data: poetData[lang],
-  poetData
-};
