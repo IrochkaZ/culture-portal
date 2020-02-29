29456d85b7fad9fb3b73b61e71b467940bfb9d02
Nikolai Shkrabkov <grr_by@mail.ru>
2020-02-29 14:48:15 +0300

    fix: fix worklog.jsx

--- src/pages/worklog.jsx
+++ src/pages/worklog.jsx
@@ -7,10 +7,13 @@ import WorklogRow from "../components/worklog-row/worklog-row";
 import Requirement from "../components/requirement/requirement";
 
 const WorklogPage = () => {
+  const windowGlobal = typeof window !== "undefined" && window;
+  const [language, setLanguage] = useState("");
   const [data, setData] = useState({});
-  const [language, setLanguage] = useState(localStorage.getItem("lang"));
+  // const [language, setLanguage] = useState(localStorage.getItem("lang"));
 
   useEffect(() => {
+    setLanguage(windowGlobal.localStorage.getItem("lang") || "ru");
     async function fetchData() {
       const response = await getMainPageData();
       setData(response);
@@ -18,11 +21,25 @@ const WorklogPage = () => {
     fetchData();
   }, []);
 
-  if (!data.length) return null;
-  const pageInfo = data.filter(el => el.fields.lang === "mainInfo");
-  const pageMainInfo = pageInfo[0].fields.data[language];
-  localStorage.setItem("lang", language);
-  localStorage.setItem("buttons", pageMainInfo.buttons);
+  if (!windowGlobal || !data.length) return null;
+  // let members = data.filter(el => el.fields.id === "members")[0].fields.data;
+  const pageInfo = data.filter(el => el.fields.lang === "mainInfo")[0].fields
+    .data;
+  // members = Object.values(members);
+  // members = members.map(item => item[language].name);
+  // let worklog = data.filter(el => el.fields.id === "worklog");
+  // worklog = Object.values(worklog[0].fields.data);
+  // const difficulties = data.filter(el => el.fields.id === "difficulties");
+  // let requirements = data.filter(el => el.fields.id === "requirements");
+  // requirements = Object.values(requirements[0].fields.data);
+
+  windowGlobal.localStorage.setItem("lang", language);
+
+  // if (!data.length) return null;
+  // const pageInfo = data.filter(el => el.fields.lang === "mainInfo");
+  // const pageMainInfo = pageInfo[0].fields.data[language];
+  // localStorage.setItem("lang", language);
+  // localStorage.setItem("buttons", pageMainInfo.buttons);
   let worklog = data.filter(el => el.fields.id === "worklog");
   worklog = Object.values(worklog[0].fields.data);
   const difficulties = data.filter(el => el.fields.id === "difficulties");
@@ -33,7 +50,11 @@ const WorklogPage = () => {
   members = members.map(item => item[language].name);
   return (
     <>
-      <Header setLanguage={e => setLanguage(e.target.value)} />
+      <Header
+        setLanguage={e => setLanguage(e.target.value)}
+        lang={language}
+        pageInfo={pageInfo}
+      />
       <h2>Worklog</h2>
       {members.map(item => {
         return (
@@ -63,12 +84,6 @@ const WorklogPage = () => {
           </div>
         );
       })}
-      {/* <p>
-        Total time:
-        {worklog.reduce((sum, item) => {
-          return sum + item.hours;
-        }, 0)}
-      </p> */}
       <h2>Difficulties</h2>
       <p>{difficulties[0].fields.data[0][language]}</p>
       <h2>Requirements</h2>
